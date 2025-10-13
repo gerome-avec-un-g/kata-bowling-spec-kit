5401beddb24d707d9878ab0c4c058906bbe0343c
parmentier.g <gerome.parmentier@gmail.com>
2025-10-13 14:27:19 +0200

    compute 0

---
+++ src/test/java/fr/geromeavecung/bowling/BowlingCalculatorTest.java
@@ -0,0 +1,16 @@
+package fr.geromeavecung.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BowlingCalculatorTest {
+
+    @Test
+    void compute_score_0() {
+        BowlingCalculator bowlingCalculator = new BowlingCalculator();
+        int score = bowlingCalculator.compute(new Frames("0 0 0 0 0 0 0 0 0 0"));
+        assertThat(score).isZero();
+    }
+
+}
